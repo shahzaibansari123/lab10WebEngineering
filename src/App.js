650617167759baa71f959b73e168d7527eb21c83
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-01-20 17:38:35 +0500

    work complete

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Router, useParams ,Routes, useRouteMatch} from "react-router";
+import { Link } from "react-router-dom";
+// import Switch from "react-router-dom"
+// import {Switch} from 'react-router-dom'
+// import useRouteMatch from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Student 1</Link>
+          </li>
+          <li>
+            <Link to="/about">Student 2</Link>
+          </li>
+          <li>
+            <Link to="/user/2">Student 3</Link>
+          </li>
+          <li>
+            <Link to="/user/1">Student 4</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Student1} />
+        <Route path="/student2" component={Student2} />
+        <Route path="/student2info/:id" component={Student2info} />
+        <Route path="/student" component={Student} />
+      </div>
+    </Router>
+  );
+}
+
+const Student1 = () => (
+  <div>
+    <h1>Shahzaib Ansari</h1>
+    <p>Here Shahzaib  Profile Page: se2019</p>
+  </div>
+);
+
+const Student2 = () => (
+  <div>
+    <h1>Shareh</h1>
+    <p>Here Shareh Profile Page: se2019</p>
+  </div>
+);
+
+const Student2info=()=>{
+  debugger;
+  const{id}= useParams();
+  console.log(id)
+  return (
+    <div>
+    <h1>Shareh</h1>
+    <p>Here Shareh Profile Page: se2019 {id}</p>
+  </div>
+  )
+}
+
+const Student=()=>{
+  const {url} = useRouteMatch()
+  return(
+    <Routes>
+      <Route path={`${url}/`} exact component={StudentGrid} />
+      <Route path={`${url}/:id`}  component={StudentDetail} />
+    </Routes>
+  )
+}
+
+const StudentGrid=()=>{
+  return(
+    <div>
+    <h1>Student Grid</h1>
+    </div>
+)}
+
+const StudentDetail=()=>{
+  const{id}= useParams();
+  console.log(id)
+  return (
+    <div>
+    <h1>Student Detail</h1>
+    <p>Student detail Page: se2019 {id}</p>
+  </div>
+  )
+}
+export default App;
